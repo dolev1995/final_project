3c4fcb6a6b2f1f8d6f2537e31d7d475272fd13e5
naamag <naamag@yhb.org.il>
2021-12-26 11:00:14 +0200

    first

---
+++ src/LogOrReg.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react'
+import './App.css';
+import {Link} from 'react-router-dom'
+
+
+class LogOrReg extends Component{
+    onClick2(){
+        console.log("onClick");
+    }
+render(){
+return(
+<div className="login_register">
+    <div className='choose'>
+    <h2> בחר:</h2>
+    </div>
+
+<Link to="/Register">
+<button type="button" className='btn_logorreg' onClick={() =>this.onClick2}>הירשם </button></Link>
+<Link to="/Login">
+<button type="button" className='btn_logorreg' onClick={() =>this.onClick2}>התחבר </button></Link>
+
+</div>
+);
+}
+
+
+}
+
+export default LogOrReg;
