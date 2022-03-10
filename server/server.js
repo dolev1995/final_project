99d6dd7370ef69d84c62c6cfb228889144273e68
dolev1995 <2dolev20@gmail.com>
2022-03-10 16:49:42 +0200

    fix bug

--- server/server.js
+++ server/server.js
@@ -7,7 +7,6 @@ const
     bodyParser = require('body-parser'),
     config = require('./config'),
     app = express();
-    app=dd;
 
 // BASE SETUP
 // ==============================================
