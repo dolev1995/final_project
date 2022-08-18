bdcd6e4c5dcb1d5e52d032d77e710d57517af463
dolev1995 <57942180+dolev1995@users.noreply.github.com>
2022-08-18 14:14:55 +0300

    Add files via upload

--- client/src/components/sidbar/Sidebar.js
+++ client/src/components/sidbar/Sidebar.js
@@ -16,8 +16,14 @@ function Sidebar() {
 
   return (
       <div className='Sidebar'>
+
+{/* <label className='divahTeacher'><small style={{marginRight: 10 + 'px'}}><a href="mailto:admin@gmail.com">שליחת מייל למורה</a></small></label> */}
+
+
         <h4>שלום אוהד</h4>
         <h1>מסך תפריט:</h1>
+      <label className='divahTeacher'><small style={{marginRight: 10 + 'px'}}><a href="mailto:admin@gmail.com"><img className='imgMail' src="http://store-images.s-microsoft.com/image/apps.4725.14294378363439842.5d3bbc47-2b55-4ca4-8cdc-5708b4da8904.71d2f53c-05e2-4406-aac6-30cfb0de432b"/>שליחת מייל למורה</a></small></label> 
+
           <ul className='SidebarList'></ul>
           <ul>
           {SidebarData && SidebarData.map((item, index) => {
